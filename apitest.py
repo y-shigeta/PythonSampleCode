a63a0b6e9fbb09e7164244d1076446ceb8bda08e
shigeta <shige.7822@gmail.com>
2022-01-04 23:23:12 +0900

    created repository

--- apitest.py
+++ apitest.py
@@ -4,14 +4,14 @@ import json
 import requests
 from bs4 import BeautifulSoup4
 
-json_open = open('/Users/yas/Desktop/VSCode-Python/SampleOffice/cpu.json', 'r')
-data2 = json.load(json_open)
+#json_open = open('/Users/yas/Desktop/VSCode-Python/SampleOffice/cpu.json', 'r')
+#data2 = json.load(json_open)
 
 url = "https://google.com"
 res = requests.get(url)
 soup = BeautifulSoup(res, 'html.parser')
 print(soup)
 
-for v in range(0, len(data2)):
-    print(data2['host'][v]['cpu_type'])
-    print(data2['host'][v]['cpu_model'])
+#for v in range(0, len(data2)):
+#    print(data2['host'][v]['cpu_type'])
+#    print(data2['host'][v]['cpu_model'])
